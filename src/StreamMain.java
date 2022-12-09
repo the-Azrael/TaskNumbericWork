cf0a0f25efe903af0ddbeae4c2a476ceddf7018b
Maksim Tusaev <tusaev.maxim@yandex.ru>
2022-12-09 13:16:17 +0400

    fix problems

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -3,12 +3,13 @@ import java.util.stream.Stream;
 
 public class StreamMain {
     private static List numbersList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-    private static Stream<Integer> stream = numbersList.stream();
 
     public static void main(String[] args) {
-        stream.filter(x -> x > 0);
-        stream.filter(x -> x % 2 == 0);
-        stream.sorted(Comparator.naturalOrder());
-        stream.forEach(System.out::println);
+        Stream<Integer> stream = numbersList.stream();
+        stream
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
     }
 }
